0c43cc43e76aa9963f9579075e41030bf7b667f0
Vallo <vallo.zinin@metropolia.fi>
2015-11-19 00:14:54 +0200

    minor videoscreen improvement

--- app/src/main/java/metropolia/fi/suondbubbles/MainActivity.java
+++ app/src/main/java/metropolia/fi/suondbubbles/MainActivity.java
@@ -3,11 +3,8 @@ package metropolia.fi.suondbubbles;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.MediaController;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.VideoView;
 
 import metropolia.fi.suondbubbles.activities.LoginActivity;
@@ -21,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         final Intent intent = new Intent(this, LoginActivity.class);
         //startActivity(intent);
         Uri intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro);
-        VideoView videoIntro = (VideoView) findViewById(R.id.videoIntro);
+        VideoView videoIntro = (VideoView)findViewById(R.id.videoIntro);
         videoIntro.setVideoURI(intro);
         videoIntro.requestFocus();
         videoIntro.start();
@@ -32,27 +29,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
