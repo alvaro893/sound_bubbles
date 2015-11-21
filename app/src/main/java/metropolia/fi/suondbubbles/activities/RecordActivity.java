3de042f77c7f4fef6eb07ca9113dad11949711d9
alvaro893 <alvarobolro@gmail.com>
2015-11-22 01:19:52 +0200

    add layout for recording and side menu in search activity

---
+++ app/src/main/java/metropolia/fi/suondbubbles/activities/RecordActivity.java
@@ -0,0 +1,30 @@
+package metropolia.fi.suondbubbles.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import metropolia.fi.suondbubbles.R;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab22);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
