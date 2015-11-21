9d54925beb5750b56317f995fb15f3ba08af4b25
Vallo <vallo.zinin@metropolia.fi>
2015-11-21 05:57:07 +0200

    added code to support logo video different resolutions

--- app/src/main/java/metropolia/fi/suondbubbles/MainActivity.java
+++ app/src/main/java/metropolia/fi/suondbubbles/MainActivity.java
@@ -5,19 +5,55 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.VideoView;
 
 import metropolia.fi.suondbubbles.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private final String DEBUG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Intent intent = new Intent(this, LoginActivity.class);
+
+        Uri intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.first_screen_480x800);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        switch(metrics.densityDpi){
+            case DisplayMetrics.DENSITY_LOW:
+                intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro);
+                Log.d(DEBUG_TAG,"DENSITY_LOW");
+                break;
+            case DisplayMetrics.DENSITY_MEDIUM:
+                intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro);
+                Log.d(DEBUG_TAG, "DENSITY_MEDIUM");
+                break;
+            case DisplayMetrics.DENSITY_HIGH:
+                intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.first_screen_480x800);
+                Log.d(DEBUG_TAG, "DENSITY_HIGH");
+                break;
+            case DisplayMetrics.DENSITY_XHIGH:
+                intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.first_screen_480x800);
+                Log.d(DEBUG_TAG, "DENSITY_XHIGH");
+                break;
+            case DisplayMetrics.DENSITY_XXHIGH:
+                intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.first_screen_480x800);
+                Log.d(DEBUG_TAG,"DENSITY_XXHIGH");
+                break;
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.first_screen_480x800);
+                Log.d(DEBUG_TAG, "DENSITY_XXXHIGH");
+                break;
+        }
+
+
         //startActivity(intent);
-        Uri intro = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro);
+
         VideoView videoIntro = (VideoView)findViewById(R.id.videoIntro);
         videoIntro.setVideoURI(intro);
         videoIntro.requestFocus();
